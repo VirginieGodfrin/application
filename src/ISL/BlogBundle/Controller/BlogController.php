0b0d85c59066944b9ecc2421041dbf4e41af88ab
Greg Berger <greg@3kd.be>
2015-02-12 18:20:47 +0100

    les templates - Twig partie 1 la syntaxe

--- src/ISL/BlogBundle/Controller/BlogController.php
+++ src/ISL/BlogBundle/Controller/BlogController.php
@@ -22,13 +22,34 @@ class BlogController extends Controller{
     
     public function indexAction($page=''){
         
+        $name = $this->get('session')->get('name');
+        
+        
+        return $this->render(
+          'ISLBlogBundle:Blog:test.html.twig',      
+          array(
+              'page' => 2,
+              'objet' => array('nom'=>'Berger', 'prenom'=>'Gregory', 'Age'=>34),
+              'html' => '<div>Hello world i\'m in a div</div>',
+              'monArray' => array(1,32,43,12),
+              'exercice' => array('Nom'=>'Berger', 'Prenom'=>'Gregory', 'Age'=>34, 'Ville'=>'Liège'),
+          )
+        );
     }
     
     public function voirAction($id){
         return new Response('<body>Article '.$id.'</body>');
     }
     
-    public function ajouterAction(){
+    public function ajouterAction(Request $req){
+       
+        $this->get('session')->getFlashBag()
+                ->add('error', 'Une erreur est survenue');
+        
+        $url = $this->generateUrl('isl_blog_index');
+        return $this->redirect($url);
+        
+        
         
     }
     
