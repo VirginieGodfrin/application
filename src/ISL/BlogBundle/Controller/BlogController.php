4072e2823c6febc1c54afb7daf903b2cb42d00c5
Greg Berger <greg@3kd.be>
2015-02-28 14:09:43 +0100

    doctrine partie 1

--- src/ISL/BlogBundle/Controller/BlogController.php
+++ src/ISL/BlogBundle/Controller/BlogController.php
@@ -11,6 +11,7 @@ namespace ISL\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use ISL\BlogBundle\Entity\Article;
 
 
 /**
@@ -22,7 +23,15 @@ class BlogController extends Controller{
     
     public function indexAction($page=''){
         
-        return $this->render('ISLBlogBundle::index.html.twig');
+        $repo = $this->getDoctrine()
+                    ->getManager()
+                    ->getRepository('ISLBlogBundle:Article');
+        // $repo = $this->get('article_repository');
+        $articles = $repo->findAll();
+        
+        return $this->render('ISLBlogBundle::index.html.twig',
+                    array('articles'=>$articles)
+                );
     }
     
     public function voirAction($id){
@@ -32,7 +41,13 @@ class BlogController extends Controller{
     public function ajouterAction(Request $req){
        
       
-       
+       $article = new Article();
+       $article->setAuteur('Berger');
+       $article->setTitre('Mon premier article');
+       $article->setContenu('Lorem ipsum dolor sit amet etc');
+       $em = $this->getDoctrine()->getManager();
+       $em->persist($article);
+       $em->flush();
         return $this->render('ISLBlogBundle:Blog:ajouter.html.twig');
         
         
@@ -50,5 +65,15 @@ class BlogController extends Controller{
         $articles = array(array('titre'=>'Article 1'),array('titre'=>'Article 2'),array('titre'=>'Article 3'),array('titre'=>'Article 4'));
         
         return $this->render('ISLBlogBundle:_partials:derniersArticles.html.twig',array('articles'=>$articles));
+    }
+    
+    public function testAction(){
+        $mots_interdits = $this->get('isl_blog.antimotsinterdits');
+        $compteur = $mots_interdits->combienDeMotsInvalides("terroriste nazi nul!");
+        if($compteur > 0){
+            throw new \Exception("Trop de mots interdits");
+            
+        }
+        return $this->render("ISLBlogBundle::index.html.twig");
     }
 }
